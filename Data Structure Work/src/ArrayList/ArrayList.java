335b9c9c77528cd0e65eeae5b0816efd65889136
jorra04 <jorra04@my.yorku.ca>
2020-12-19 13:26:01 -0500

    added more features for the ArrayList class.

---
+++ Data Structure Work/src/ArrayList/ArrayList.java
@@ -0,0 +1,105 @@
+package ArrayList;
+
+import java.util.Arrays;
+
+public class ArrayList<E> {
+	private Object[] list;
+	private int size;
+	private static final int INIT_CAP = 10;
+	
+	public ArrayList() {
+		this.size = 0;
+		this.list = new Object[INIT_CAP];
+	}
+	
+	public int size() {
+		return this.size;
+	}
+	
+	public Object get(int index) {
+		try {
+			return list[index];
+		}
+		catch(Exception e) {
+			throw new IndexOutOfBoundsException("The given index was out of bounds");
+			
+		}
+	}
+	
+	public void add(E item) {
+		if(this.list.length == this.size) {
+			ensureCapacity();
+		}
+		this.list[this.size++] = item;
+	}
+	
+	public void remove(E item) {
+		if(this.size <= this.list.length /2) {
+			reduceCapacity();
+		}
+		int temp_size = this.size;
+		for(int i = 0; i < temp_size; i ++) {
+			if(this.list[i].equals(item)) {
+				this.list[i] = null;
+				this.size --;
+			}
+		}
+		int index = 0;
+		for(Object element : this.list) {
+			if( element != null) {
+				list[index++] = element;
+			}
+		}
+	}
+	
+	public void removeIndex(int index) {
+		try {
+			if(this.size <= this.list.length /2) {
+				reduceCapacity();
+			}
+			this.list[index] = null;
+			this.size--;
+			int dummy_index = 0;
+			for(Object element : this.list) {
+				if( element != null) {
+					list[dummy_index++] = element;
+				}
+			}
+		}
+		catch(Exception e) {
+			throw new IndexOutOfBoundsException("Index out of bounds");
+		}
+	}
+	
+	public boolean contains(E item) {
+		for(int i = 0; i < this.size; i ++) {
+			if(this.list[i].equals(item)) return true;
+		}
+		
+		return false;
+	}
+	@Override
+	public String toString() {
+		String output = "";
+		for(int i = 0; i < this.size; i ++) {
+			if(i == this.size -1) {
+				output += this.list[i];
+			}
+			else {
+				output += this.list[i] + ", ";
+			}
+		}
+		return output;
+	}
+	
+	private void ensureCapacity() {
+		int new_size = this.list.length * 2;
+		Arrays.copyOf(this.list, new_size);
+	}
+	private void reduceCapacity() {
+		int new_size = this.list.length /2;
+		Arrays.copyOf(this.list, new_size);
+	}
+	
+
+}
---
+++ Data Structure Work/src/ArrayList/ArrayList_tester.java
@@ -0,0 +1,152 @@
+package ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ArrayList_tester {
+
+	@Test
+	void add_test01() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		
+		assertEquals(5, myList.get(0));
+	}
+	
+	@Test
+	void add_test02() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		
+		assertEquals(4, myList.get(1));
+	}
+	
+	@Test
+	void add_test03() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		
+		assertEquals(3, myList.get(2));
+	}
+	
+	@Test
+	void size_test01() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		
+		assertEquals(3, myList.size());
+	}
+	@Test
+	void size_test02() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		myList.add(4);
+		assertEquals(2, myList.size());
+	}
+	@Test
+	void size_test03() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		assertEquals(1, myList.size());
+	}
+	
+	@Test
+	void contains_test01() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		
+		assertEquals(true, myList.contains(4));
+	}
+	
+	@Test
+	void contains_test02() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		
+		assertEquals(false, myList.contains(1));
+	}
+	
+	@Test
+	void removeAll_test01() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+	
+		
+		myList.remove(3);
+		
+		assertEquals(false, myList.contains(3));
+	}
+	
+	@Test
+	void removeAll_test02() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		
+		
+		myList.remove(5);
+		
+		assertEquals(false, myList.contains(5));
+	}
+	
+	@Test
+	void removeIndex_test01() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		
+		
+		myList.removeIndex(2);
+	
+		assertEquals(false, myList.contains(3));
+		assertEquals(2, myList.size());
+	}
+	
+	@Test
+	void removeIndex_test02() {
+		ArrayList<Integer> myList = new ArrayList();
+		myList.add(5);
+		myList.add(4);
+		myList.add(3);
+		
+		
+		myList.removeIndex(2);
+		
+	
+		assertEquals(false, myList.contains(3));
+		assertEquals(2, myList.size());
+		myList.removeIndex(0);
+		assertEquals(false, myList.contains(5));
+		assertEquals(1, myList.size());
+	}
+
+}
