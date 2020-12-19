5fa1f6153a34e9c3cecdb6882f56cffd206291b8
jorra04 <jorra04@my.yorku.ca>
2020-12-19 15:27:35 -0500

    Added some additional methods and fixed up the documentation.

--- Data Structure Work/src/LinkedList/LinkedList.java
+++ Data Structure Work/src/LinkedList/LinkedList.java
@@ -4,35 +4,81 @@ public class LinkedList {
 	private Node head;
 	private Node tail;
 	private int size;
-	
+
 	LinkedList(){
 		this.head = null;
 		this.tail = this.head;
 		this.size = 0;
 	}
-	
+	/*
+	 * Section 1. Methods responsible for adding to the LinkedList
+	 * contains push and append.
+	 */
 	public void push(Object item) {
-		
+
 		Node node = new Node(item);
 		node.next = head;
 		head = node;
 		if(this.size == 0) tail = head;
 		this.size ++;
-		
 	}
 	
 	public void append(Object item) {
 		Node node = new Node(item);
+		if(this.size == 0) {
+			this.head = this.tail = node;
+		}
+		
 		this.tail.next = node;
 		this.tail = node;
 		this.size ++;
 	}
 	
+	/*
+	 * Section 2. Methods responsible for removing from the LinkedList
+	 * contains removeFirst and removeLast.
+	 */
 	
+	public void removeFirst() {
+		this.head = this.head.next;
+		this.size --;
+	}
+	
+	public void removeLast() {
+		Node curr = head;
+		while(curr.next.next != null) {
+			curr = curr.next;
+		}
+		tail = curr;
+		tail.next = null;
+	}
+	
+
+	/*
+	 * Section 3. Methods in this section are responsible for getting data
+	 * about the data structure.
+	 */
 	public int size() {
 		return this.size;
 	}
 	
+	public boolean isEmpty() {
+		return this.size == 0;
+	}
+	
+	public Object first() {
+		return this.head.value;
+	}
+	
+	public Object last() {
+		return this.tail.value;
+	}
+	
+	/*
+	 * Section 4. This section is responsible for keeping tabs on the DS and ensuring 
+	 * it is behaving to our expectations.
+	 */
+
 	public void printList() {
 		Node curr = this.head;
 		while(curr != null) {
@@ -42,10 +88,10 @@ public class LinkedList {
 			else {
 				System.out.print(curr.value + ", ");
 			}
-			
+
 			curr = curr.next;
 		}
 	}
-	
-	
+
+
 }
--- Data Structure Work/src/LinkedList/LinkedListTester.java
+++ Data Structure Work/src/LinkedList/LinkedListTester.java
@@ -11,7 +11,10 @@ class LinkedListTester {
 		LinkedList list = new LinkedList();
 		list.push(5);
 		list.push(4);
-		
+		/*
+		 * List looks like..
+		 * [5]->[4]-> null
+		 */
 		assertEquals(2, list.size());
 	}
 	
@@ -23,7 +26,90 @@ class LinkedListTester {
 		list.push(5);
 		list.append(3);
 		list.append(2);
+		
+		/*
+		 * List looks like..
+		 * [5]->[4]->[3]->[2]-> null
+		 */
+		
 		assertEquals(4, list.size());
+	}
+	
+	@Test
+	void getFirst_test01() {
+		LinkedList list = new LinkedList();
+		
+		list.push(5);
+		list.push(6);
+		
+		/*
+		 * List looks like..
+		 * [5]->[6]-> null
+		 */
+		assertEquals(6, list.first());
+	}
+	
+	@Test
+	void getFirst_test02() {
+		LinkedList list = new LinkedList();
+		
+		list.append(5);
+		list.append(6);
+		list.push(4);
+		
+		/*
+		 * List looks like..
+		 * [4]->[5]->[6]-> null
+		 */
+		
+		assertEquals(4, list.first());
+	}
+	
+	@Test
+	void removeFirst_test01() {
+		LinkedList list = new LinkedList();
+		
+		list.append(5);
+		list.append(6);
+		list.push(4);
+		
+		/*
+		 * List looks like..
+		 * [4]->[5]->[6]-> null
+		 */
+		
+		list.removeFirst();
+
+		/*
+		 * List looks like..
+		 * [5]->[6]-> null
+		 */
+		
+		assertEquals(5, list.first());
+	}
+	
+	@Test
+	void removeLast_test01() {
+		LinkedList list = new LinkedList();
+		
+		list.push(4);
+		list.push(5);
+		list.append(3);
+		list.append(2);
+		
+		/*
+		 * List looks like..
+		 * [5]->[4]->[3]->[2]-> null
+		 */
+		
+		list.removeLast();
+		
+		/*
+		 * List looks like..
+		 * [5]->[4]->[3]-> null
+		 */
+	
+		assertEquals(3, list.last());
 	}
 
 }
